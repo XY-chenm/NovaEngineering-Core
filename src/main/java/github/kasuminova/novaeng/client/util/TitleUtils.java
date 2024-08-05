0286ec597b5a873a1ba663e970018583dd5a76d3
XY-chenm <103646169+XY-chenm@users.noreply.github.com>
2024-08-06 02:18:20 +0800

    修改标题硬编码部分（）

--- src/main/java/github/kasuminova/novaeng/client/util/TitleUtils.java
+++ src/main/java/github/kasuminova/novaeng/client/util/TitleUtils.java
@@ -12,8 +12,8 @@ public class TitleUtils {
     /**
      * TODO 喜欢我硬编码吗.jpg
      */
-    public static final String DEFAULT_TITLE = "Nova Engineering: World 1.13.1 by Hikari_Nova | Core Ver: " + NovaEngineeringCore.VERSION;
-    public static final String VANILLA_TITLE = "Minecraft 1.12.2";
+    public static final String DEFAULT_TITLE = "SKLH by chenm | QQ群：850051274  ";
+    public static final String VANILLA_TITLE = "柒周目-1.12.2";
 
     public static String currentTitle = null;
     public static String lastCurrentTitle = null;
